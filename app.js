84370a7d9c8665019fedb56a5627e2d57bc086eb
pvinaymadhur <vinaymadhur14@gmail.com>
2021-11-02 21:08:21 -0500

    car and its attributes are added

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var rollsroyceRouter = require('./routes/rollsroyce');
+
+
 
 var app = express();
 
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/rollsroyce', rollsroyceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ readme.md
@@ -0,0 +1,18 @@
+# fwb27madhur
+
+Webapps Assignment-10 (fwb27madhur)
+
+[Link](https://fwb27madhur.herokuapp.com/)
+
+Class RollsRoyce
+
+RollsRoyce_type(string)
+Model(string)
+Cost(number)
+
+<!-- [RollsRoyce](https://fwb27madhur.herokuapp.com/rollsRoyce)
+
+[Addmods](https://fwb27madhur.herokuapp.com/addmods?rows=3&cols=4)
+
+[Selector](https://fwb27madhur.herokuapp.com/selector) -->
+
