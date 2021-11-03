1fdc17d7d85aee481ada1f0c14688beba9408f98
pvinaymadhur <vinaymadhur14@gmail.com>
2021-11-02 21:43:10 -0500

    added rollsroyce images and wrote random function

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var rollsroyceRouter = require('./routes/rollsroyce');
 var addmodsRouter = require('./routes/addmods');
-
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -26,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/rollsroyce', rollsroyceRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
