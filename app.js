ec495eedabb90569df948db34931c237d740f87d
pvinaymadhur <vinaymadhur14@gmail.com>
2021-11-02 21:18:11 -0500

    addmods file is created

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var rollsroyceRouter = require('./routes/rollsroyce');
-
+var addmodsRouter = require('./routes/addmods');
 
 
 var app = express();
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/rollsroyce', rollsroyceRouter);
+app.use('/addmods', addmodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
